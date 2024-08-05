bb33ebae122e3b6992172c450c6c985e3d554688
Harindersidhu007 <harindersidhu071@gmail.com>
2024-08-05 22:13:55 +0530

    url,routing and creating new movie controller

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -12,11 +12,29 @@ namespace WebApplication2_DepDsgEmp_1147
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("Home/contact");
+            //routes.IgnoreRoute("Home/about");
+            //routes.IgnoreRoute("Employee/{*pathInfo}");
+
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            name: "MyRoute",
+            url: "cs/dotnettraining",
+            defaults: new { controller = "Test", action = "abc" }
+            );
+
+
+            routes.MapRoute(
+           name: "Default",
+           url: "{controller}/{action}/{id}",
+           defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
+
+                routes.MapRoute(
+                name: "movieroute",
+                url: "Movie/ReleaseDate/{year}/{month}",
+                defaults: new { controller = "movie", action = "DateOfMovie" },
+                constraints: new { month = @"\d{2}", year = @"\d{4}" }
             );
         }
     }
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2_DepDsgEmp_1147.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DateofMovie(int year,int month)
+        {
+            return Content(year.ToString() + "/" + month.ToString());
+        }
+
+    }
+}
